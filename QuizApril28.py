bbeb360a4eb22372e324409abd93269b6bff4af4
Student <student@ms.boyslatin.org>
2023-04-28 09:32:41 -0400

    first commit

---
+++ QuizApril28.py
@@ -0,0 +1,15 @@
+#1. A block of code which only runs when it is called. (2)When trying to turn a whole number into a fraction 
+#2. A Method is used to operate the data in the class and a function is used to return pass the data
+#3. A dictionary is a unordered collection that contains key pairs separted by commas inside the brackets.(2)to find a word 
+#4. Github is a code hosting platform for version control and collaboration. (2) It lets you and others work together on projects from anywhere 
+#5. Classes are for bundling related data while dictionaries are for storing key-value relationships
+ cities={"philadelphia, LA, Brooklyn, london, chicago"}
+
+#8.
+#9.
+#10. A code might have an error because a missing quatation mark, or extra spacebar, or no parenthesis 
+#11-
+def function(name):
+    print (hello, name. How are you today?)
+           
+The code didnt work because a missing quatation mark before the hello and after the print
